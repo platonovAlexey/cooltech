f04acb8b530cd88e0a4011b47e0e5c0c5359707e
platonovAlexey <kalyawe4ka@gmail.com>
2016-02-19 02:25:49 +0300

    add nav and breadcrumbs

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,1 +1,28 @@
 console.log('in app.js');
+
+var ctegoriesSort = (function(){
+
+    var _changeActiveClass = function($this){
+        $this.closest(".categories__item, .nav__item").addClass("active")
+            .siblings().removeClass("active");
+    }
+
+    return {
+        init: function(){
+            $(".categories__link, .nav__link").on("click", function(e){
+                e.preventDefault();
+                 _changeActiveClass($(this));
+            });
+        }
+    }
+}());
+
+
+	$(document).ready(function (){
+
+		ctegoriesSort.init();
+
+
+	});
+
+
