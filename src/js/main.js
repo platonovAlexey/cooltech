f5a5357a825221f423d89108c27c5acb849994f1
platonovAlexey <kalyawe4ka@gmail.com>
2016-02-20 15:43:56 +0300

    add sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
  */
 
 //= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/jquery-ui/jquery-ui.js
 
 
 
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,26 +1,109 @@
 console.log('in app.js');
+var sliderWidget = (function(){
 
-var ctegoriesSort = (function(){
+    var _insertValues = function($this, min, max){
+        var container = $this.closest(".filter__slider"),
+            from = container.find(".filter__slider_input_from"),
+            to = container.find(".filter__slider_input_to");
 
-    var _changeActiveClass = function($this){
-        $this.closest(".categories__item, .nav__item").addClass("active")
-            .siblings().removeClass("active");
-    }
+        from.val(min);
+        to.val(max);
+    };
 
     return {
         init: function(){
-            $(".categories__link, .nav__link").on("click", function(e){
-                e.preventDefault();
-                 _changeActiveClass($(this));
+
+            $(".filter__slider_element").each(function(){
+
+                var $this = $(this),
+                    min = parseInt($this.data("min")),
+                    max = parseInt($this.data("max"));
+
+                $this.slider({
+                    range: true,
+                    min: min,
+                    max: max,
+                    values: [min, max],
+                    slide: function(evt, ui) {
+                        var values = ui.values;
+                        _insertValues($this, values[0], values[1]);
+                    },
+                    create: function() {
+                        var values = $this.slider("option", "values");
+                        _insertValues($this, values[0], values[1]);
+                    }
+                });
             });
         }
     }
 }());
 
 
+var ctegoriesSort = (function(){
+
+	var _changeActiveClass = function($this){
+		$this.closest(".categories__item, .nav__item").addClass("active")
+			.siblings().removeClass("active");
+	}
+
+	return {
+		init: function(){
+			$(".categories__link, .nav__link").on("click", function(e){
+				e.preventDefault();
+				 _changeActiveClass($(this));
+			});
+		}
+	}
+}());
+
+var accordeon = (function(){
+	var _openSection = function($this){
+		var container = $this.closest(".filter__item"),
+			content = container.find(".filter__content"),
+			otherContant = $this.closest(".filter").find(".filter__content");
+
+		if (!container.hasClass("active")){
+			otherContant.slideUp().closest(".filter__item").removeClass("active");
+
+
+			container.addClass("active");
+			content.stop(true, true).slideDown();
+		}else{
+			container.removeClass("active");
+			content.stop(true, true).slideUp();
+		}
+	}
+
+	return {
+		init: function(){
+			$(".filter__title ").on("click", function(e){
+				e.preventDefault();
+				_openSection($(this));
+			});
+		}
+	}
+}());
+
+
 	$(document).ready(function (){
 
 		ctegoriesSort.init();
+		accordeon.init();
+		sliderWidget.init();
+
+		$(".filter__reset").on("click", function(e){
+		e.preventDefault();
+
+		var $this = $(this),
+			container = $this.closest(".filter__item"),
+			checkboxes = container.find("input:checkbox");
+
+		checkboxes.removeAttr("checked")
+		});
+
+		if ($(".filter__slider_element").length){
+        	sliderWidget.init();
+    	}
 
 
 	});
